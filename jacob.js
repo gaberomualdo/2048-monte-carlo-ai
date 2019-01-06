838f3da83985e436bb9c535035c2e55f7fac457f
xtrp <36894883+xtrp@users.noreply.github.com>
2019-01-06 00:33:22 +0000

    bugs

--- jacob.js
+++ jacob.js
@@ -1,12 +1,10 @@
 var gamesPerMove = 50;
 var totalMoveScores = [0,0,0,0];
-var totalMoveCount = 0;
 var totalGamesDone = 0;
 var paused = false;
 function makeMove(){
 	totalMoveScores = [0,0,0,0];
 	totalGamesDone = 0;
-	totalMoveCount = 0;
 	
 	["left","right","up","down"].forEach(function(move, move_index){
 		for(var games = 0;games < gamesPerMove;games++){
@@ -18,16 +16,17 @@ function simulateRun(move, move_index){
 	var simulation = getNewGameObj();
 	simulation.board = [game.board[0].slice(0), game.board[1].slice(0), game.board[2].slice(0), game.board[3].slice(0)];
 	simulation.main_move(move);
-	while(simulation.check_gameover() == false){
-		simulation.main_move(["left","right","up","down"][Math.floor(Math.random() * 4)]);
-		totalMoveCount += 1;
+	if(JSON.stringify(simulation.board) != JSON.stringify(game.board)){
+		while(simulation.check_gameover() == false){
+			simulation.main_move(["left","right","up","down"][Math.floor(Math.random() * 4)]);	
+		}
+		totalMoveScores[move_index] += simulation.getScore();
 	}
-	totalMoveScores[move_index] += simulation.getScore();
 	totalGamesDone += 1;
 	if(totalGamesDone >= gamesPerMove * 4){
 		var bestMove = ["left","right","up","down"][totalMoveScores.indexOf(Math.max(...totalMoveScores))];
 		game.main_move(bestMove);
-		document.querySelector("div.console").innerHTML += ("<p>Best move was " + bestMove.toUpperCase() + " with average score of " + parseFloat(Math.max(...totalMoveScores) / gamesPerMove).toFixed(2) + ".<br>Average move count on all runs was " + parseFloat(totalMoveCount / gamesPerMove).toFixed(2) + " with " + gamesPerMove + " simulations per move.</p>");
+		document.querySelector("div.console").innerHTML += ("<p>Best move was " + bestMove.toUpperCase() + " with an average score of " + parseFloat(Math.max(...totalMoveScores) / gamesPerMove).toFixed(2) + " and " + gamesPerMove + " simulations per move.</p>");
 		(function(){
 			var elem = document.querySelector("div.console");
 			elem.scrollTop = elem.scrollHeight;
